e17fa0e8e84839f60c0bdbe97ab34b76f9d9809b
powertalent <powertalentstars@gmail.com>
2023-09-02 19:41:33 +0700

    Update voz.user.js

--- voz.user.js
+++ voz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VOZ
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Me
 // @match        https://voz.vn/*
