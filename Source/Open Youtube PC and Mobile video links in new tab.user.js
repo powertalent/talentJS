3001e115f61b52514aaa2b9a349e2d4f44ce9b72
powertalent <powertalentstars@gmail.com>
2023-10-18 07:47:43 +0700

    update

---
+++ Source/Open Youtube PC and Mobile video links in new tab.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Open Youtube PC and Mobile video links in new tab
+// @description  A Helper Script to open Youtube PC and Mobile video links in new tab
+// @include      https://www.youtube.com/*
+// @include      https://m.youtube.com/*
+// @exclude      https://www.youtube.com/watch*
+// @exclude      https://m.youtube.com/watch*
+// @namespace    Open Youtube PC and Mobile video links in new tab
+// @author       -
+// @version      2.0.4
+// @license      MIT License
+// @grant        GM_openInTab
+// @run-at       document-start
+// ==/UserScript==
+
+var suppressing;
+window.addEventListener('mouseup', function(e) {
+	if (e.button > 1 || e.altKey)
+		return;
+	var link = e.target.closest('[href^="/watch"]');
+	if (!link ||
+		(link.getAttribute('href') || '').match(/^(javascript|#|$)/) ||
+		link.href.replace(/#.*/, '') == location.href.replace(/#.*/, '')
+	)
+		return;
+
+	GM_openInTab(link.href, e.button || e.ctrlKey);
+	suppressing = true;
+	prevent(e);
+}, true);
+
+window.addEventListener('click', prevent, true);
+window.addEventListener('auxclick', prevent, true);
+
+function prevent(e) {
+	if (!suppressing)
+		return;
+	e.preventDefault();
+	e.stopPropagation();
+	e.stopImmediatePropagation();
+	setTimeout(function() {
+		suppressing = false;
+	}, 100);
+}
